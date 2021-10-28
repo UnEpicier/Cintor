3ccd9d90469ba77624de2ab8cc1af4243d9b2e4f
GABLRM <gabriel.laroumanie@ynov.com>
2021-10-28 17:18:45 +0200

    LOgin

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 require('dotenv').config();
-const { Client, Intents } = require ('discord.js');
+const { Client, Intents, Message } = require ('discord.js');
 const client = new Client({ intents : [Intents.FLAGS.GUILDS] });
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
+
+	const { commandName } = interaction;
+
+	if (commandName === 'ping') {
+		await interaction.reply('Pong!');
+	} else if (commandName === 'server') {
+		await interaction.reply('Server info.');
+	} else if (commandName === 'user') {
+		await interaction.reply('User info.');
+	}
 });
 
 client.login(process.env.CLIENT_TOKEN);
