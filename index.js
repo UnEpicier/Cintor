69ee111788f8f342df620dea1378c45aa967d745
AVASSEUR2 <alexis.vasseur@ynov.com>
2021-10-28 18:00:38 +0200

    Ping Test Command

--- index.js
+++ index.js
@@ -1,27 +1,68 @@
-require('dotenv').config();
-const { Client, Intents, Message } = require ('discord.js');
+const fs = require('fs');
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
+const { Client, Intents, Collection } = require ('discord.js');
+
 const client = new Client({ intents : [Intents.FLAGS.GUILDS] });
 
-client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`);
-});
+const dotenv = require('dotenv');
+dotenv.config();
+const TOKEN = process.env['TOKEN'];
+const TEST_GUILD_ID = process.env['TEST_GUILD_ID'];
+
+const commandsFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+const commands = [];
+client.commands = new Collection();
+
+for (const file of commandsFiles) {
+	const command = require(`./commands/${file}`);
+	commands.push(command.data.toJSON());
+	client.commands.set(command.data.name, command);
+}
 
 client.once('ready', () => {
-	console.log('Ready!');
+    console.log(`Ready!`);
+
+	const CLIENT_ID = client.user.id;
+	const rest = new REST({
+		version: '9'
+	}).setToken(TOKEN);
+
+	(async () => {
+		try {
+			if (!TEST_GUILD_ID) {
+				await rest.put(
+					Routes.applicationCommands(CLIENT_ID), {
+						body: commands
+					},
+				);
+				console.log('Successfully registered application commands globally');
+			} else {
+				await rest.put(
+					Routes.applicationGuildCommands(CLIENT_ID, TEST_GUILD_ID), {
+						body: commands
+					},
+				);
+				console.log('Successfully registered application commands for development guild');
+			}
+		} catch (error) {
+			if (error) console.error(error)
+		}
+	})();
 });
 
 client.on('interactionCreate', async interaction => {
 	if (!interaction.isCommand()) return;
 
-	const { commandName } = interaction;
+	const command = client.commands.get(interaction.commandName);
+	if (!command) return;
 
-	if (commandName === 'ping') {
-		await interaction.reply('Pong!');
-	} else if (commandName === 'server') {
-		await interaction.reply('Server info.');
-	} else if (commandName === 'user') {
-		await interaction.reply('User info.');
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		if (error) console.error(error);
+		await interaction.reply({ content : 'There was an error while executing this command!', ephemeral: true});
 	}
 });
 
-client.login(process.env.CLIENT_TOKEN);
+client.login(TOKEN);
